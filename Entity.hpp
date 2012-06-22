af67880bec863f9836bc3d78afc58bd0f929c4da
Stephen Pridham <stephenap07@gmail.com>
2012-06-22 05:49:50 -0400

    Deleted crappy files

--- Entity.hpp
+++ Entity.hpp
@@ -3,18 +3,9 @@
  */
 
 
-class Entity
+class IEntity
 {
 public:
   virtual void Think(float dt) = 0;   
-  virtual void AddProcess(const Process & proc) = 0; 
 };
 
-class PhysicsEntity : public Entity, PhysicsObject
-{ 
-public: 
-  virtual void Think(float dt) = 0; 
-  virtual void AddProcess(const Process &proc) = 0; 
-
-    
-}; 
