dfa565d8f2e501ebd8da8431910dbc30a8f80322
Stephen Pridham <stephenap07@gmail.com>
2012-06-28 09:50:45 -0400

    Improve makefile to be more generic, Add build/run script to make debugging faster. Initial commit for breakout test

--- makefile
+++ makefile
@@ -1,18 +1,21 @@
-CXX     = g++
+CXX     = g++ 
 config	= debug
 LDFLAGS = -lsfml-system -lsfml-graphics -lsfml-window -lsfml-audio
 
 # Optimize if not in debug mode 
 ifeq ($(config), release) 
-	CFLAGS	= -c -Wall -std=gnu++0x -O2
+	CFLAGS	= -c -Wall -std=gnu++11 -O2
 	OUTEXE  = bin/release/sfml-app
 else  
-	CFLAGS	= -c -Wall -std=gnu++0x
+	CFLAGS	= -c -Wall -std=gnu++11
 	OUTEXE  = bin/debug/sfml-app
 endif
 
+SRCDIR  := src
+SRCEXT   = cpp
+
 SYMLINK  = run
-SOURCES  = common/*.cpp 
+SOURCES := $(shell find $(SRCDIR) -type f -name *.$(SRCEXT))
 OBJDIR   = obj
 OBJFILES = $(addprefix $(OBJDIR)/, $(notdir $(SOURCES:.cpp=.o)))
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,132 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
+#include <iostream>
+#include <functional>
+
+#define BLOCK_WIDTH_SIZE 10 
+#define BLOCK_HEIGHT_SIZE 2.5 
+
+typedef int tag; 
+
+struct CollisionMesage { 
+  sf::FloatRect collision_bounds;  
+  tag id; 
+
+  CollisionMesage(sf::FloatRect bounds, tag id=-1) : id(id) {}
+  CollisionMesage() : id(-1) {}
+}; 
+
+
+class BlockMap { 
+  private:
+    std::vector<sf::RectangleShape> _renderObjects;
+    int id;  
+  public:
+    BlockMap(float offset_x, float offset_y, int count, std::function<sf::Vector2f(int)> f) 
+    {
+      id = 0; 
+      for(int i=0; i<count; ++i) { 
+        _renderObjects.push_back(sf::RectangleShape(sf::Vector2f(BLOCK_WIDTH_SIZE, BLOCK_HEIGHT_SIZE)));
+        _renderObjects[i].setPosition(f(i)); 
+        _renderObjects[i].move(offset_x, offset_y); 
+      }
+    }
+
+    const std::vector<sf::RectangleShape> & getRenderObjects() {
+      return _renderObjects;
+    }
+
+    // Return object ids and collision bounds for whatever 
+    const std::vector<CollisionMesage> getCollisionMessages(const sf::FloatRect collbox) { 
+      std::vector<CollisionMesage> messages; 
+      for( auto & block : _renderObjects ) { 
+       if( block.getGlobalBounds().intersects(collbox) ) { 
+          CollisionMesage message; 
+          messages.push_back(message); 
+          message.collision_bounds = block.getGlobalBounds();  
+          message.id = 0; 
+       }
+      }
+      return messages; 
+    }
+
+    void handleCollision(const CollisionMesage & message) { 
+    }
+};
+
+class Ball 
+{
+  private: 
+    sf::RectangleShape _ball; 
+    float _speedX; 
+    float _speedY; 
+
+  public: 
+    Ball(float x, float y) :_speedX(0), _speedY(15.0f) 
+    {
+      _ball = sf::RectangleShape(sf::Vector2f(2.0f, 2.0f));    
+      _ball.setPosition(x,y); 
+    }
+
+    const sf::RectangleShape getRenderObject() {
+      return _ball; 
+    }
+
+    void think(float dt) { 
+      _ball.move(_speedX*dt, _speedY*dt);  
+    }
+
+    const sf::FloatRect getCollisionBounds() { 
+      return _ball.getGlobalBounds(); 
+    }
+
+    CollisionMesage getCollisionMessages(const sf::FloatRect & collbox) { 
+      //check if the two boxes collide 
+      CollisionMesage message; 
+      if(_ball.getGlobalBounds().intersects(collbox)) { 
+        message.collision_bounds = _ball.getGlobalBounds(); 
+        message.id = 1; 
+        
+        // handle collision here
+         
+      }
+
+      return message; 
+    }
+}; 
 
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(300, 200), "SFML works!"); 
+  sf::RenderWindow window(sf::VideoMode(300, 200), "breakout fool!"); 
+  sf::Clock Clock; 
 
   //Rectangle object 
   
+  float  width          =  10.0f;
+  float  height         =  2.5f;
+  float  offset_vert    =  -20.0f;
+  float  offset_horiz   =  -width/2.0f;
+  float  middle_screen  =  window.getView().getSize().x/2;
+  float  end_screen     =  window.getView().getSize().y;
+  float  initial_y      =  end_screen + offset_vert;
+  float  initial_x      =  middle_screen + offset_horiz;
+
   sf::RectangleShape rectangle;
-  rectangle.setSize(sf::Vector2f(5,5));
+  rectangle.setSize(sf::Vector2f(width,height));
   rectangle.setOutlineColor(sf::Color::Red);
   rectangle.setOutlineThickness(1);
-  rectangle.setPosition(10, 20);
+  rectangle.setPosition(initial_x, initial_y);
 
-  sf::Clock clock; 
-  sf::Time time; 
-  
-  int side = 1; 
-  int updown = 0; 
+  sf::RectangleShape collided(sf::Vector2f(10,10));
+  collided.setFillColor(sf::Color::Blue); 
 
-  int left;
-  int right;
-  int up;    
-  int down;   
-
-  float speed = 5.0f; 
+  BlockMap map(100, 30, 10, [](int i) -> sf::Vector2f{ 
+          return sf::Vector2f(i*BLOCK_WIDTH_SIZE, (i % 2)*BLOCK_HEIGHT_SIZE ); 
+        }); 
+  Ball ball(90, 50); 
 
   while (window.isOpen())
   {  
-    time = clock.restart(); 
 
     sf::Event event;
     while (window.pollEvent(event))
@@ -35,17 +134,29 @@ int main()
       if (event.type == sf::Event::Closed)
         window.close();
     }
-    
-    side = left  ? -1 : side; 
-    side = right ?  1 : side; 
 
-    updown = up   ? -1 : updown; 
-    updown = down ?  1 : updown; 
+    float mouse_x = sf::Mouse::getPosition(window).x;
+    float dt = Clock.getElapsedTime().asSeconds(); 
+    Clock.restart(); 
 
-    rectangle.move(speed*side*time.asSeconds(), speed*updown*time.asSeconds()); 
+    rectangle.setPosition(sf::Vector2f(mouse_x, initial_y)); 
+    ball.think(dt); 
+    std::vector<sf::RectangleShape> blocks = map.getRenderObjects(); 
+
+    sf::FloatRect rect = ball.getCollisionBounds(); 
+    if( map.getCollisionMessages(rect).size() ) { 
+    }
+    else { 
+    }
+    if( ball.getCollisionMessages(rectangle.getGlobalBounds()).id >= 0 ) { 
+    }
 
     window.clear();
     window.draw(rectangle);
+    window.draw(ball.getRenderObject()); 
+    for( auto block : map.getRenderObjects() ) { 
+      window.draw(block); 
+    }
     window.display();
   }
 
