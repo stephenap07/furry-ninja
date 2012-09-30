83debe61aeb22889125c2db4fe831542dc5dde78
Stephen Pridham <stephenap07@gmail.com>
2012-09-30 08:44:59 -0400

    add platformer include directory

---
+++ include/Constants.h
@@ -0,0 +1,39 @@
+#pragma once
+
+
+const int WINDOW_WIDTH = 640;
+const int WINDOW_HEIGHT = 480;
+
+const int TILE_WIDTH = 32;
+const int TILE_HEIGHT = 32; 
+const int TILE_SIZE = 32; 
+
+// 20x15 tiles
+const int MAP_WIDTH = WINDOW_WIDTH/TILE_WIDTH;
+const int MAP_HEIGHT = WINDOW_HEIGHT/TILE_HEIGHT;
+
+const int TILE_TOTAL = 32;
+
+const int PI = 3.14159; 
+
+const float GRAVITY = 15.0f;
+
+const int MAX_BACON = 64; 
+const int MAX_LASERS = 64; 
+const int PALLETE_MAX = 64; 
+
+const int PLAYER_WIDTH = 19;
+const int PLAYER_HEIGHT = 26; 
+
+const int MAX_ANIMS = 2; 
+const int MAX_FRAMES = 12; 
+
+enum LAYERS
+{
+	COLLISION =0,
+	MAIN,
+	ENTITIES,
+	BACKGROUND,
+
+	TOTAL
+};
