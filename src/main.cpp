b12391c43c7c293686baf7cc00f4791a87eea7a4
Stephen Pridham <stephenap07@gmail.com>
2012-10-05 17:19:39 -0400

    move Animation system into it's own header file

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 
 // My indludes
 #include "../include/Constants.h"
+#include "../include/AnimationManager.h"
 
 //Textures
 sf::Texture tileset;        
@@ -56,96 +57,6 @@ int collisionMap[MAP_HEIGHT][MAP_WIDTH];
 sf::Sprite pallete[PALLETE_MAX]; 
 int numTiles; 
 
-// Animation Typedefs 
-typedef sf::IntRect frame_t; 
-typedef std::vector<frame_t> animation_t; ; 
-
-struct Animation { 
-  Animation(const animation_t & a, float t = 0.05f, int cur_frame=0) 
-  : _anim(a), _time_limit(t), _current_frame(cur_frame), _current_time(0.0f) {}; 
-
-  void setAnimation(const animation_t & a) { 
-    _anim = a; 
-  }
-
-  const frame_t & getNextFrame(float dt) { 
-    _current_time += dt;  
-
-    if(_current_time > _time_limit) { 
-      _current_time = 0.0f; 
-      if(++_current_frame > (_anim.size()-1)) {
-        restart(); 
-      }
-    }
-    
-    return _anim[_current_frame]; 
-  }
-
-  const frame_t & getFrame(int index) { 
-    if( index < _anim.size() ) { 
-      return _anim[index]; 
-    }
-    else { 
-      std::cout << "Programmer error\n"; 
-    }
-  }
-
-  void restart() { 
-    _current_frame = 0; 
-    _current_time = 0.0f; 
-  }
-
-  private: 
-    animation_t _anim;         
-    int _current_frame; 
-    float _current_time; 
-    float _time_limit; 
-};
-
-class AnimationManager { 
-  private: 
-
-    // Variables
-    std::vector<Animation> _animations; 
-    int _current_anim; 
-
-    const frame_t & getNextFrame(int anim, float dt) { 
-      if(_current_anim <= (_animations.size()-1)) {
-        return _animations[_current_anim].getNextFrame(dt); 
-      }
-      else { 
-        std::cout << "programmer error\n"; 
-      }
-    }
- public: 
-    AnimationManager() :_current_anim(0) { }
-    int pushAnim(const Animation &anim) { 
-      _animations.push_back(anim); 
-      return (_animations.size()-1); 
-    }
-    void setAnim(int anim) { 
-      if( _current_anim != anim ) { 
-        if(_current_anim <= (_animations.size()-1)) {
-          _animations[_current_anim].restart(); 
-        }
-        _current_anim = anim; 
-      }
-    }
-    int getAnim() { 
-      return _current_anim; 
-    }
-    const frame_t & getFrame(int index) { 
-      if(_current_anim < _animations.size()) { 
-        return _animations[_current_anim].getFrame(index); 
-      }
-    }
-    const frame_t & play(int anim, float dt) { 
-      setAnim(anim); 
-      return getNextFrame(anim, dt);
-    }
-
-};
-
 class Entity { 
   public:
     virtual void think(float dt) {}; 
@@ -162,19 +73,19 @@ class Player : public DrawableEntity
 {
 
 private:
+
 	//animations indexes
-	enum { ANIM_RIGHT=0, ANIM_LEFT,
-         ANIM_IDLE
-       };
+  enum { ANIM_RIGHT=0, ANIM_LEFT, ANIM_IDLE };
+
   int _current_direction; 
 
   AnimationManager _anim_mgr; 
 
   // private attributes
-	int m_jumpPosition;
-	bool m_jumpKey;
+	int _jumpPosition;
+	bool _jumpKey;
 	int jumpSpeed;;
-	bool m_lockJump; 
+	bool _lockJump; 
 
 	//sounds
 	sf::Sound m_soundJump;
@@ -218,9 +129,17 @@ public:
 
 	//functions
 	
-	Player() :_current_direction(ANIM_RIGHT), m_lockJump(false), velX(0), velY(0),
-            nextthink(0.01f), jumpSpeed(5), m_jumpPosition(0), m_jumpKey(false),
-            physicsObject(true), inuse(false)	
+	Player() :
+  _current_direction(ANIM_RIGHT), 
+  _lockJump(false), 
+  velX(0), 
+  velY(0),
+  nextthink(0.01f), 
+  jumpSpeed(5), 
+  _jumpPosition(0), 
+  _jumpKey(false),
+  physicsObject(true), 
+  inuse(false)	
   {
 		//set the texture
 		sprite.setTexture(tileset);
@@ -256,14 +175,14 @@ public:
 			velX = -220;
 		}else velX = 0;
 
-		if( sf::Keyboard::isKeyPressed(sf::Keyboard::Z) && m_lockJump == false && grounded ) {
-			m_lockJump = true;
+		if( sf::Keyboard::isKeyPressed(sf::Keyboard::Z) && _lockJump == false && grounded ) {
+			_lockJump = true;
 			m_soundJump.play();
 		}
 
-		if(m_lockJump) { //if we pressed the jump button
+		if(_lockJump) { //if we pressed the jump button
 			Jump();
-			m_lockJump = false;
+			_lockJump = false;
 		}
 
     if(velX > 0) {
