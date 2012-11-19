66c0933d0132b29d94da9a0ffc5678994fd0e8d6
Stephen Pridham <stephenap07@gmail.com>
2012-11-19 11:25:23 -0500

    intitial commit for player refactoring

--- include/Player.hpp
+++ include/Player.hpp
@@ -3,17 +3,73 @@
 
 #include "Entity.hpp"
 
+class PlayerModel { 
+
+  private: 
+    AnimationManager _anim_mgr; 
+
+   void setFrame(const frame_t frame) { 
+      sprite->setTextureRect(frame);
+    } 
+   
+   void setAnimation(const int anim, float dt) { 
+      setFrame(_anim_mgr.play(anim, dt)); 
+    }
+
+  public: 
+    sf::Sprite *sprite; 
+    
+    enum { ANIM_RIGHT=0, ANIM_LEFT, ANIM_IDLE };
+
+    PlayerModel() { 
+      //set up animation frames
+      _anim_mgr.pushAnim( Animation( 
+            {  
+              frame_t(0, 65, PLAYER_WIDTH, PLAYER_HEIGHT), 
+              frame_t(PLAYER_WIDTH, 65, PLAYER_WIDTH, PLAYER_HEIGHT),
+              frame_t(PLAYER_WIDTH*2, 65, PLAYER_WIDTH, PLAYER_HEIGHT)
+            }, 0.075f )
+          );
+
+      _anim_mgr.pushAnim( Animation( 
+            {  
+              frame_t(0, 91, PLAYER_WIDTH, PLAYER_HEIGHT),
+              frame_t(PLAYER_WIDTH, 91, PLAYER_WIDTH, PLAYER_HEIGHT),
+              frame_t(PLAYER_WIDTH*2, 91, PLAYER_WIDTH, PLAYER_HEIGHT)
+            }, 0.075 )
+          );
+    }
+    
+    void UpdateAnimations(const int anim, float dt)
+    {
+      switch(anim) { 
+        case ANIM_RIGHT: 
+         setAnimation(ANIM_RIGHT, dt);
+          break; 
+
+        case ANIM_LEFT: 
+          setAnimation(ANIM_LEFT,dt);
+          break; 
+
+        case ANIM_IDLE: 
+          if(_anim_mgr.getAnim() == ANIM_RIGHT) { 
+            setFrame(_anim_mgr.getFrame(0)); 
+          }
+          else {
+            setFrame(_anim_mgr.getFrame(2)); 
+          }
+          break; 
+      } 
+    }
+};
+
 class Player : public DrawableEntity
 {
 
 private:
 
-	//animations indexes
-  enum { ANIM_RIGHT=0, ANIM_LEFT, ANIM_IDLE };
-
+  PlayerModel player_model; 
   int _current_direction; 
-
-  AnimationManager _anim_mgr; 
 
   // private attributes
 	int _jumpPosition;
@@ -26,24 +82,6 @@ private:
 	sf::Sound m_soundPickUp;
 
 private:
-
-	void UpdateAnimations(float dt)
-	{
-		if(_current_direction == ANIM_RIGHT) {
-			sprite.setTextureRect(_anim_mgr.play(ANIM_RIGHT,dt));
-		}
-		else if(_current_direction == ANIM_LEFT) {
-			sprite.setTextureRect(_anim_mgr.play(ANIM_LEFT,dt));
-		}
-    else if(_current_direction == ANIM_IDLE) { 
-      if(_anim_mgr.getAnim() == ANIM_RIGHT) { 
-        sprite.setTextureRect(_anim_mgr.getFrame(0)); 
-      }
-      else {
-        sprite.setTextureRect(_anim_mgr.getFrame(2)); 
-      }
-    }
-	}
 
 	void Jump()
 	{
@@ -64,7 +102,7 @@ public:
 	//functions
 	
 	Player(sf::Texture & tileset, sf::SoundBuffer & jump, sf::SoundBuffer &pickup) :
-  _current_direction(ANIM_RIGHT), 
+  _current_direction(PlayerModel::ANIM_RIGHT), 
   _lockJump(false), 
   velX(0), 
   velY(0),
@@ -78,23 +116,10 @@ public:
 		//set the texture
 		sprite.setTexture(tileset);
 
-		//set up animation frames
-		_anim_mgr.pushAnim( Animation( {  frame_t(0, 65, PLAYER_WIDTH, PLAYER_HEIGHT), 
-                                      frame_t(PLAYER_WIDTH, 65, PLAYER_WIDTH, PLAYER_HEIGHT),
-                                      frame_t(PLAYER_WIDTH*2, 65, PLAYER_WIDTH, PLAYER_HEIGHT)
-                                   }, 0.075f )
-                      );
+    player_model.sprite = &sprite; 
+    player_model.UpdateAnimations(PlayerModel::ANIM_RIGHT, 0.0f);
 
-		_anim_mgr.pushAnim( Animation( {  frame_t(0, 91, PLAYER_WIDTH, PLAYER_HEIGHT),
-                                      frame_t(PLAYER_WIDTH, 91, PLAYER_WIDTH, PLAYER_HEIGHT),
-                                      frame_t(PLAYER_WIDTH*2, 91, PLAYER_WIDTH, PLAYER_HEIGHT)
-                                   }, 0.075 )
-                      );
-
-		//set the current animations and current frame
-    sprite.setTextureRect(_anim_mgr.play(ANIM_RIGHT,0.0f)); 
-
-		//set up sounds
+    //set up sounds
 		m_soundJump.setBuffer(jump);
 		m_soundPickUp.setBuffer(pickup);	
 	}
@@ -120,17 +145,16 @@ public:
 		}
 
     if(velX > 0) {
-      _current_direction = ANIM_RIGHT; 
+      _current_direction = PlayerModel::ANIM_RIGHT; 
     }
     else if(velX < 0) {
-      _current_direction = ANIM_LEFT; 
+      _current_direction = PlayerModel::ANIM_LEFT; 
 		}
     else { 
-      _current_direction = ANIM_IDLE; 
+      _current_direction = PlayerModel::ANIM_IDLE; 
     }
 
-		UpdateAnimations(dt);
-
+    player_model.UpdateAnimations(_current_direction, dt);
 	}
 
 };
